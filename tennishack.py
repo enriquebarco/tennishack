89c2cb9838c1c79e442da6a0279fb03e8245b360
Enrique Barco <93683516+enriquebarco@users.noreply.github.com>
2022-11-21 00:23:00 -0500

    Update tennishack.py

--- tennishack.py
+++ tennishack.py
@@ -16,7 +16,7 @@ booking_url = os.getenv('TENNISHACK_BOOKING_URL')
 binary_location = os.getenv('GOOGLE_CHROME_BIN')
 chrome_driver_path = os.getenv('CHROMEDRIVER_PATH')
 
-# make window max size
+# make window max size, chrome and chrome driver settings to run headless on heroku
 chrome_options = Options()
 chrome_options.add_argument('--kiosk')
 chrome_options.binary_location = binary_location
@@ -29,6 +29,8 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path,  options=chrome_op
 # driver = webdriver.Chrome(options=chrome_options)
 wait = WebDriverWait(driver, 20)
 driver.get(url)
+
+print('page loaded')
 
 # input login information
 username_el = driver.find_element(By.XPATH,'//*[@id="p_lt_ContentWidgets_pageplaceholder_p_lt_zoneContent_CHO_Widget_LoginFormWithFullscreenBackground_XLarge_loginCtrl_BaseLogin_UserName"]')
@@ -44,6 +46,8 @@ print('logged in')
 # navigate to tennis bookings post authentication
 driver.get(booking_url)
 
+print('succesfully navigated to booking page')
+
 # show courts available two days ahead
 wait.until(EC.frame_to_be_available_and_switch_to_it(driver.find_element(By.XPATH,'//*[@id="module"]')))
 wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[1]/div/div/div/div[3]/div/div[5]/div[1]/div/div/div')))
@@ -52,7 +56,7 @@ driver.execute_script("arguments[0].click();", two_days_advance_div)
 
 print('navigated to correct date')
 
-# select court available at 7pm court 6
+# select court available at 7pm
 wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[1]/div/div/div/div[3]/div/div[5]/div[1]/div/div/div')))
 wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[1]/div/div/div/div[3]/div/div[5]/div[1]/div/div/div/div[6]/div/div/div[2]')))
 wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[1]/div/div/div/div[3]/div/div[5]/div[1]/div/div/div/div[6]/div/div/div[2]/div[18]')))
