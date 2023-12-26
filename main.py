2f2c3474f596e9c8851e1ea1a783a253b3b31bd8
Enrique Barco <enrique.barco96@hotmail.com>
2023-12-26 01:56:53 -0500

    added back heorku requirments

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ binary_location = os.getenv('GOOGLE_CHROME_BIN')
 # make window max size, chrome settings to run headless on heroku. To run localy, comment out chrome_options.binary_location and chrome_options.add_argument('--headless')
 chrome_options = Options()
 chrome_options.add_argument('--kiosk')
-# chrome_options.binary_location = binary_location
-# chrome_options.add_argument('--headless')
+chrome_options.binary_location = binary_location
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
@@ -40,6 +40,10 @@ def main ():
         wait_until_8am_est()
         paddle.run()
     else:
+        paddle = PaddleCourtBooking(driver, wait)
+        paddle.login()
+        wait_until_8am_est()
+        paddle.run()
         tennis = TennisCourtBooking(driver, wait)
         tennis.login()
         wait_until_8am_est()
